91724b1854c37d130bddbafafc392b224db6338e
Harish <harish.snvs@gmail.com>
2020-04-19 11:09:19 +0530

    added project files

---
+++ src/test/java/ics/First.java
@@ -0,0 +1,16 @@
+package ics;
+
+import org.testng.annotations.Test;
+
+import junit.framework.TestCase;
+
+public class First extends TestCase {
+	
+	public static void main(String args[]) {
+		System.out.println("Hi this is first");
+		
+	}
+	
+	
+
+}
