714ad1fe9be3b1193821b62447ee209b0e142db1
Harish <harish.snvs@gmail.com>
2020-04-19 11:19:30 +0530

    Some changes

--- src/test/java/ics/First.java
+++ src/test/java/ics/First.java
@@ -7,7 +7,7 @@ import junit.framework.TestCase;
 public class First extends TestCase {
 	
 	public static void main(String args[]) {
-		System.out.println("Hi this is first");
+		System.out.println("Hi this is first test");
 		
 	}
 	
